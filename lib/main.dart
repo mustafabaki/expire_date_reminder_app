ed8907eed580fa03789c9f4d0e2a75760882c66c
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2022-02-07 12:22:35 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,27 @@
 // @dart=2.9
 
 import 'package:flutter/material.dart';
-import 'package:sqflite/sqflite.dart';
 import 'homepage.dart';
 import 'database.dart';
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 
 void main() async {
   runApp(const myApp());
 
   WidgetsFlutterBinding.ensureInitialized();
   final dbHelper = DatabaseHelper.instance;
+  // initialize the alarm manager
+  AndroidAlarmManager.initialize();
+
+  //check periodically the expiration dates of the products
+
+  AndroidAlarmManager.periodic(const Duration(days: 1), 1, notify);
+
+}
+
+void notify(){
+// TODO: complete this function for notification functionality.
+
 }
 
 class myApp extends StatefulWidget {
